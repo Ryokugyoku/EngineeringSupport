662eac722f370d5094c7c1be0dd4d455de2e6062
ryokugyoku <ryoku@blueremarks.com>
2026-01-30 23:04:39 +0900

    Add Velopack integration and macOS build workflow
    - Integrated Velopack into WinUI and macOS projects.
    - Added macOS-specific build workflow in GitHub Actions.
    - Updated release workflow to include macOS artifacts.

--- EngineeringSupport.Mac/Main.cs
+++ EngineeringSupport.Mac/Main.cs
@@ -1,5 +1,6 @@
 using ObjCRuntime;
 using UIKit;
+using Velopack;
 
 namespace EngineeringSupport.Mac;
 
@@ -8,6 +9,9 @@ public class Program
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
+        VelopackApp.Build()
+            .Run();
+
         // if you want to use a different Application Delegate class from "AppDelegate"
         // you can specify it here.
         UIApplication.Main(args, null, typeof(AppDelegate));
